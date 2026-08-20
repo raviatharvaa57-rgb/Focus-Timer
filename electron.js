2f8c74aae7346a46471be8e6fda32fb29d91cab3
raviatharvaa57-rgb <raviatharvaa57@gmail.com>
2026-08-20 21:20:22 +1000

    Fix black screen using relative paths and dist load

--- electron.js
+++ electron.js
@@ -5,15 +5,18 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    title: "Focus Timer",
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
 
-  const startUrl = process.env.ELECTRON_START_URL || `file://${path.join(__dirname, 'dist/index.html')}`;
-  win.loadURL(startUrl);
+  // Load the built dist/index.html file in production
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, 'dist', 'index.html'));
+  } else {
+    win.loadURL('http://localhost:5173');
+  }
 }
 
 app.whenReady().then(createWindow);
